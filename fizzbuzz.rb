c0baa9207b07b11f77697070c3c5f917c244676d
Tiffany <ong.tifffany1503@gmail.com>
2018-07-04 04:40:16 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-1.upto(100) do |int|
+def fizzbuzz (int)
 if int % 3 == 0 && int % 5 == 0
   puts "FizzBuzz"
 elsif int % 3 == 0

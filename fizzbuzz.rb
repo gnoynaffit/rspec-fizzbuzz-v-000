7d1df6af7f08b8505b4bb563fbdc3d81f5bd75f9
Tiffany <ong.tifffany1503@gmail.com>
2018-07-04 04:37:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz (i)
-if i % 3 == 0 && i % 5 == 0
+def fizzbuzz (int)
+if int % 3 == 0 && i % 5 == 0
   puts "FizzBuzz"
-  elsif i % 3 == 0
+elsif int % 3 == 0
     puts "Fizz"
-  elsif i % 5 == 0
+  elsif int % 5 == 0
     puts "Buzz"
 
 

b9c62758cbc6002aed0ee32ef03c8963efbc7eb4
Tiffany <ong.tifffany1503@gmail.com>
2018-07-04 05:38:25 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,19 +2,16 @@
 # See README.md for instructions on how to do this
 def fizzbuzz (int)
 
-
- elsif int % 3 == 0 && int % 5 == 0
+  if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
 
-    if int % 3 == 0
+  elsif int % 3 == 0
       "Fizz"
 
-    elsif int % 5 == 0
+  elsif int % 5 == 0
       "Buzz"
 
- else
-
-
+  else
 
  end
 end
